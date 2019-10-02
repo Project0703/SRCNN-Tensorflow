30ab9a5b75c2f702036fee7a7d88126851e65e0b
Project0703 <45531439+Project0703@users.noreply.github.com>
2019-10-02 07:05:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ flags.DEFINE_integer("c_dim", 1, "Dimension of image color. [1]")
 flags.DEFINE_integer("scale", 3, "The size of scale factor for preprocessing input image [3]")
 flags.DEFINE_integer("stride", 14, "The size of stride to apply input image [14]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Name of checkpoint directory [checkpoint]")
-flags.DEFINE_string("sample_dir", "Train", "Name of sample directory [sample]")
+flags.DEFINE_string("sample_dir", "sample", "Name of sample directory [sample]")
 flags.DEFINE_boolean("is_train", True, "True for training, False for testing [True]")
 FLAGS = flags.FLAGS
 
